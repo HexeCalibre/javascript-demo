e88cf53426798646eb6e7e1417546b8ac84f7136
IamIronMan2023 <kramsotnas@yahoo.com>
2023-07-05 19:02:09 +0800

    First Commit

---
+++ script.js
@@ -0,0 +1,52 @@
+// alert("Hello World!");
+
+// ----Console functions----
+// console.log("Hello World!");
+// console.warn("warning message");
+// console.debug("debug message");
+// console.dir();
+// console.error("error message");
+
+//----Variables-----
+var firstName = "Juan";
+var lastName = "Dela Cruz";
+
+var firstName = "John";
+
+console.log(firstName);
+console.log(lastName);
+
+let fName = "John";
+let lName = "Doe";
+
+fName = "Jose";
+lName = "Rizal";
+
+console.log(fName);
+console.log(lName);
+
+const address = "Malacanang";
+//address = "White House"; //error here
+
+//----Data Types---
+
+const fullName = "Juan Dela Cruz"; //String
+const age = 20; // Number Integer
+const weight = 88.1; // Number float or decimal
+const isOverweight = true; //true or false
+const undef = undefined;
+const nullValue = null;
+
+console.log("\nData Type typeof");
+
+console.log(typeof fullName);
+console.log(typeof age);
+console.log(typeof weight);
+console.log(typeof isOverweight);
+console.log(typeof undef);
+console.log(typeof nullValue);
+
+let v = "hello";
+console.log(v);
+v = 10;
+console.log(V);
